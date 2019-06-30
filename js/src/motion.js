3b75160a4e0ab49b0f20b9dd8b27abc657ebbbd6
吴宏飞 <hongfei.wu@binqsoft.com>
2019-07-01 00:20:00 +0800

    Site updated: 2019-07-01 00:19:59

--- js/src/motion.js
+++ js/src/motion.js
@@ -362,6 +362,16 @@ $(document).ready(function () {
         }
       }
     },
+    sidebarToggle: function (integrator) {
+      sidebarToggleMotion.init();
+      integrator.next();
+      // 可以不加if直接显示
+      // 加if的话，要在主题配置文件里面修改 sidebar: always
+      if (CONFIG.sidebar === 'always') {
+        // 模拟点击一下侧边栏按钮
+        sidebarToggleMotion.clickHandler();
+      }
+    },
     sidebar: function (integrator) {
       if (CONFIG.sidebar.display === 'always') {
         NexT.utils.displaySidebar();
